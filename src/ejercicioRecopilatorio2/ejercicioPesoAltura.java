6154278f725930428b5518761288e0c00ecc6d2f
Freper <codewithfreper@gmail.com>
2025-06-25 12:14:52 +0200

    Comienzo ejercicio recopilatorio2

---
+++ src/ejercicioRecopilatorio2/ejercicioPesoAltura.java
@@ -0,0 +1,20 @@
+package ejercicioRecopilatorio2;
+
+import java.util.Scanner;
+
+public class ejercicioPesoAltura {
+
+	public static void main(String[] args) {
+		/* Crea un programa que introduciendo el sexo y la altura devuelva el peso ideal. 
+		H = altura - 110; M = altura - 120 */
+		
+		Scanner lector = new Scanner(System.in);
+		String sexo;
+		int altura;
+		double peso;
+		
+		System.out.println("Por favor,introduce tu sexo (H/M):");
+		sexo = lector.nextLine();
+		
+	}
+}
